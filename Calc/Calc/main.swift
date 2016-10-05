32f09ac90323ca4336aeb3cde5a52c7f0c7477fb
hamzahaly <hzhaly93@gmail.com>
2016-10-05 15:30:45 -0700

    working on decimals

--- Calc/Calc/main.swift
+++ Calc/Calc/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  Calc
 //
-//  Created by iGuest on 10/4/16.
-//  Copyright © 2016 iGuest. All rights reserved.
+//  Created by Hamzah Aly on 10/4/16.
+//  Copyright © 2016 Hamzah Aly. All rights reserved.
 //
 
 import Foundation
