f5da5ba74f486d16842f18bdc96901a1559116c7
hamzahaly <hzhaly93@gmail.com>
2016-10-05 15:06:27 -0700

    removed some print statements

--- Calc/Calc/main.swift
+++ Calc/Calc/main.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-print("Hello, World!")
-
-print("git test")
-
 let resp1 = readLine(strippingNewline: true)!
 let num1 = Int.init(resp1)!
 var nextResp = readLine(strippingNewline: true)!
@@ -30,27 +26,28 @@ if nextResp == "fact" {
         operand -= 1
     }
     print(fact)
+    
 // if the user wants to do a multiple number operation
 } else if (Int.init(nextResp) != nil) {
     
     // The next input from the user should be a number
-    //var nextResp = readLine(strippingNewline: true)!
-    //var nextNum = Int.init(resp2)! // Change response into an integer
     nextNum = Int.init(nextResp)!
     var count = 1;
     var sum = num1;
     var avg: Double = 0;
     // While we are still recieving numbers from the user
     while (Int.init(nextResp) != nil) {
-        print("we got a number response")
+        //print("we got a number response")
         count += 1
         sum += nextNum
         nextResp = readLine(strippingNewline: true)!
+        
         // User wants to do a count operation
         if nextResp == "count" {
             print("Let's count how many numbers there are")
             print("Count: " + "\(count)" )
         }
+            
         // User wants to do an average operation
         else if nextResp == "avg" {
             print("Let's find the average of all the numbers")
@@ -60,9 +57,10 @@ if nextResp == "fact" {
             avg = Double(sum) / Double(count)
             print("Average: " + "\(avg)")
         }
+            
         // User wants to input more numbers
         else {
-            print("We got a number response")
+            //print("We got a number response")
             nextNum = Int.init(nextResp)!
         }
     }
@@ -72,7 +70,6 @@ if nextResp == "fact" {
     // print("we got an operation response")
     
     let resp3 = readLine(strippingNewline: true)!
-    
     let num3 = Int.init(resp3)!
     
     switch nextResp {
